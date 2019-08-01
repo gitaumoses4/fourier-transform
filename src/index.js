e5f6e7f63c3185701c38814a50abb0bcef27a761
Moses Gitau <gitaumoses4@gmail.com>
2019-08-01 09:04:11 +0300

    Move home to root

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 import App from './routes';
+import Home from './views/Home';
 
-const routes = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-ReactDOM.render(routes(), document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
